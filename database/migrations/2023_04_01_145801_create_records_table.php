923312cd4cb33f933855868e529b90b21f39458d
Simone Marino <simo.marino@outlook.it>
2023-04-01 17:04:22 +0200

    Table records migrated

---
+++ database/migrations/2023_04_01_145801_create_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('album', 50);
+            $table->string('author', 30);
+            $table->year('year');
+            $table->string('editor')->nullable();
+            $table->string('length');
+            $table->text('poster')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+};
